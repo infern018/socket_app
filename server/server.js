4c36c33aa6390c8836a312a843f128f0199abe96
Vishwas Singh <singh.vishu234@gmail.com>
2020-10-08 10:50:17 +0530

    [Add]: emit/broadcasting events

--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
-const path = require("path");
 const express = require("express");
+
+var app = express();
+var http = require('http').createServer(app);
+var path = require('path');
+var io = require('socket.io')(http);
 
 const publicPath = path.join(__dirname,"/../public");
 const port = process.env.PORT || 3000
-var app = express();
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on("connection", (socket) => {
+    console.log("a user is connected");
+
+    socket.emit('newMessage', {
+        from:"Admin",
+        text:"Welcome to the room",
+        createdAt : new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from:"Admin",
+        text:"A new user has joined",
+        createdAt : new Date().getTime()
+    });
+
+    
+
+    socket.on('createMessage',(message) => {
+        io.emit('newMessage', {
+            from:message.from,
+            text: message.text,
+            createdAt : new Date().getTime()
+        })
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+})
+
+http.listen(port, ()=> {
     console.log("server is running on port " + port);
 });
